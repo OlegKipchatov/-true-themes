27a9f85589843c27c2a29a8ccd7c551e15f54b1f
Oleg Kipchatov <o.kipchatov@tinkoff.ru>
2024-12-26 18:06:47 +0300

    ci: create nx monorepo

---
+++ library/README.md
@@ -0,0 +1,7 @@
+# true-themes
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test true-themes` to execute the unit tests via [Vitest](https://vitest.dev/).
---
+++ library/src/__tests__/true-themes.spec.tsx
@@ -0,0 +1,10 @@
+// import { render } from '@testing-library/react';
+
+// import TrueThemes from '../index';
+
+// describe('TrueThemes', () => {
+//   it('should render successfully', () => {
+//     const { baseElement } = render(<TrueThemes />);
+//     expect(baseElement).toBeTruthy();
+//   });
+// });
