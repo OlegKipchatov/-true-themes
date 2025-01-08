c6992a828fd8afdcbe8907dfbd454075b0d1b4a5
Oleg Kipchatov <o.kipchatov@tinkoff.ru>
2025-01-08 17:20:13 +0300

    ci: fix build

--- libs/true-themes/vite.config.ts
+++ libs/true-themes/vite.config.ts
@@ -33,7 +33,7 @@ export default defineConfig({
     },
     lib: {
       // Could also be a dictionary or array of multiple entry points.
-      entry: 'src/index.ts',
+      entry: 'src/index.tsx',
       name: 'true-themes',
       fileName: 'index',
       // Change this to the formats you want to support.
